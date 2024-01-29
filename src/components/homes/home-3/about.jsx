877a8df412f9cb449ab729aaec15363db1caa464
GuruF <guruprasath@aidaptive.com>
2024-01-29 23:44:46 +0530

    About Card

--- src/components/homes/home-3/about.jsx
+++ src/components/homes/home-3/about.jsx
@@ -11,21 +11,21 @@ const About = () => {
           <div className="row align-items-center">
             <div className="col-lg-6 wow tpfadeUp" data-wow-delay=".3s">
 
-            <Tilt
-              className="tilt-img"
-              tiltMaxAngleX={5}
-              tiltMaxAngleY={5}
-              perspective={900}
-              transitionSpeed={1000}
-              gyroscope={true}
+              <Tilt
+                className="tilt-img"
+                tiltMaxAngleX={5}
+                tiltMaxAngleY={5}
+                perspective={900}
+                transitionSpeed={1000}
+                gyroscope={true}
               >
-              <div
-                className="da-about-img mb-30"
-                data-tilt=""
-                data-tilt-perspective="2000"
-              >
-                <img src="/assets/img/about/da-about-1.jpg" alt="about-img" />
-              </div>
+                <div
+                  className="da-about-img mb-30"
+                  data-tilt=""
+                  data-tilt-perspective="2000"
+                >
+                  <img src="/assets/img/about/da-about-1.jpg" alt="about-img" />
+                </div>
               </Tilt>
 
 
@@ -34,12 +34,10 @@ const About = () => {
               <div className="tp-da-about-info mb-30">
                 <div className="section-title-wraper">
                   <div className="tp-section">
-                    <span className="tp-section__subtitle mb-15 shadow-none text-grey p-0">
-                      Since From 2000
-                    </span>
                     <h2 className="tp-section__title mb-30">
-                      Good design means good <span>business.</span>{" "}
+                      Ignite your business's success with compelling <span>solutions.</span>{" "}
                     </h2>
+
                   </div>
                 </div>
                 <div className="tp-da-about-fea pb-25 mb-10">
@@ -48,20 +46,20 @@ const About = () => {
                       <span>
                         <i className="fal fa-check"></i>
                       </span>
-                      Features include Multi Factor Authentication
+                      Features include Strategic Social Media Marketing
                     </li>
                     <li>
                       <span>
                         <i className="fal fa-check"></i>
                       </span>
-                      We strongly believe in providing open source solutions
+                      We strongly believe in providing innovative digital solutions
                     </li>
                   </ul>
                 </div>
                 <div className="tp--indo-btns d-flex flex-wrap align-items-center">
                   <div className="tp-da-link-btn mr-30 mb-30">
                     <Link href="/contact" className="tp-grd-btn">
-                      get in touch
+                      connect with us
                       <span className="ml-10">
                         <i className="fal fa-long-arrow-right"></i>
                         <i className="fal fa-long-arrow-right"></i>
@@ -70,6 +68,7 @@ const About = () => {
                   </div>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
